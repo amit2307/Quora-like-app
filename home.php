06ce95c5fa5713ab3a9f1c4e8b2ffa3566728a0c
amit2307 <akranjan2307@gmail.com>
2019-02-15 04:15:43 +0530

    First commit

---
+++ home.php
@@ -0,0 +1,25 @@
+<?php
+
+session_start();
+if(!isset($_SESSION['username'])){
+    header('location:login.php');
+}
+?>
+<html>
+<head>
+<title>Home page</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+</head>
+<body>
+    
+    <div class="container">
+
+    <a class="float-right" href="logout.php">LOGOUT</a>
+    <h1>Welcome <?php echo $_SESSION['username']; ?> </h1>
+
+
+    </div>
+    
+</body>
